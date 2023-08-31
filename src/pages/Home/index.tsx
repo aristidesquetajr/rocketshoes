0b72e5bcd1f249db3eaf216a5bcd6cb00e7995f0
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-09-01 00:32:43 +0100

    feat: created method to receive the list of products via api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,54 +1,45 @@
+import { useEffect, useState } from 'react'
 import { MdAddShoppingCart } from 'react-icons/md'
 
 import styles from './styles.module.scss'
+import { Product } from '../../types'
+import { api } from '../../services/api'
+import { formatPrice } from '../../util/format'
+
+type IProduct = Omit<Product, 'amount'>
 
 export function Home() {
+  const [products, setProducts] = useState<IProduct[]>([])
+
+  useEffect(() => {
+    async function loadProducts() {
+      const { data } = await api.get<IProduct[]>('products')
+      setProducts(data)
+    }
+
+    loadProducts()
+  }, [])
+
   return (
     <ul className={styles.productList}>
-      <li>
-        <img
-          src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg"
-          alt="Tênis de Caminhada Leve Confortável"
-        />
-        <strong>Tênis de Caminhada Leve Confortável</strong>
-        <span>R$ 179,90</span>
-        <button type="button">
-          <div data-testid="cart-product-quantity">
-            <MdAddShoppingCart size={16} color="#FFF" />0
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg"
-          alt="Tênis de Caminhada Leve Confortável"
-        />
-        <strong>Tênis de Caminhada Leve Confortável</strong>
-        <span>R$ 179,90</span>
-        <button type="button">
-          <div data-testid="cart-product-quantity">
-            <MdAddShoppingCart size={16} color="#FFF" />0
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg"
-          alt="Tênis de Caminhada Leve Confortável"
-        />
-        <strong>Tênis de Caminhada Leve Confortável</strong>
-        <span>R$ 179,90</span>
-        <button type="button">
-          <div data-testid="cart-product-quantity">
-            <MdAddShoppingCart size={16} color="#FFF" />0
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+      {products.map((product) => {
+        return (
+          <li key={product.id}>
+            <img
+              src={product.image}
+              alt={product.title}
+            />
+            <strong>{product.title}</strong>
+            <span>{formatPrice(product.price)}</span>
+            <button type="button">
+              <div data-testid="cart-product-quantity">
+                <MdAddShoppingCart size={16} color="#FFF" />0
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        )
+      })}
     </ul>
   )
 }
